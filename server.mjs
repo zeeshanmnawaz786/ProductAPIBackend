3adc9c9b61a86adbc73b94d7edb6a19915cea5ad
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-08-01 10:16:59 +0500

    some changes

--- server.mjs
+++ server.mjs
@@ -7,11 +7,6 @@ config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-// app.use(cors());
 app.use(express.json());
 
 // URI
