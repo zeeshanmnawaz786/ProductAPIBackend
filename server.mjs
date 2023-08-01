cd7083dea98e9c3c9e9c9ff5db31b59e1ca60ece
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-08-01 10:01:49 +0500

    resolving cors issue

--- server.mjs
+++ server.mjs
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://smitproducts.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json());
 
 // URI
