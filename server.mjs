76b627b1e72634884ac8f8c8530f7fb192a37593
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-08-01 10:04:03 +0500

    changing

--- server.mjs
+++ server.mjs
@@ -8,12 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://smitproducts.netlify.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use(cors());
 app.use(express.json());
 
 // URI
